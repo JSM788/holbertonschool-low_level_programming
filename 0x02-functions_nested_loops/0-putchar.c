06df19b99b9c5f5ca5edd37095be7528f65f0347
Juan Salinas <salinasjuan788@gmail.com>
2022-02-08 11:01:52 -0800

    Write a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <main.h>
+
+	int main(void)
+	{
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+	putchar ('_');
+	for (i = 0; i < 7; i++)
+	{
+	putchar('p');
+	for (i = 0; i < 6; i++)
+	{
+	putchar('u');
+	for (i = 0; i < 5; i++)
+	{
+	putchar('t');
+	for (i = 0; i < 4; i++)
+	{
+	putchar('c');
+	for (i = 0; i < 3; i++)
+	{
+	putchar('h');
+	for (i = 0; i < 2; i++)
+	{
+	putchar('a');
+	for (i = 0; i < 1; i++)
+	{
+	putchar('r');
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+	putchar(10);
+	return (0);
+}
