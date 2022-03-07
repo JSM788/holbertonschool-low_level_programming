d063359d621b9d4e0397151f0b3833830e66c2a1
Juan Salinas <salinasjuan788@gmail.com>
2022-03-07 10:39:46 -0800

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,9 @@
+# ifndef _PERSONALIZAR_LIBRARIES_
+# definir  _CUSTOMIZE_LIBRARIES_
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+# endif
