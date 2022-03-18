1558c803b9ac6ea72d77f4227e1155c448a3c7df
JSM788 <salinasjuan788@gmail.com>
2022-03-17 23:36:39 -0500

    Write a function that prints You're beat! and yet, you must allow,\nI bore my house upon my back

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - check the code
+ */
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
